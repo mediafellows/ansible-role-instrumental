8f558f0c1ef14949e0a48df80b87ddb5fc35df46
Stefan Horning <stefan@hornings.de>
2017-06-08 10:26:24 +0200

    More specs

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -7,5 +7,9 @@ describe "Instrumentald setup" do
 
   describe file('/etc/instrumentald.toml') do
     it { sould be_file }
+
+    its(:content) { should include("project_token = #{ANSIBLE_VARS.fetch('instrumental_token', 'FAIL')}") }
+    its(:content) { should include('system = ["cpu", "disk", "load", "memory", "network", "swap"]') }
+    its(:content) { should include('docker = []') }
   end
 end
